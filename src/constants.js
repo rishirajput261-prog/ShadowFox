483849689136a8159e3b2b6a46b5b78f71e684a8
rishirajput261-INF <rishirajput261@gmail.com>
2026-05-16 15:55:59 +0530

    Initial commit

---
+++ src/constants.js
@@ -0,0 +1,262 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import gsapLogo from './assets/tech_logo/gsap.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import mcLogo from './assets/tech_logo/mc.png';
+import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import postgreLogo from './assets/tech_logo/postgre.png';
+import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import Shadowfoxlogo from './assets/company_logo/Shadowfox_logo.jpg';
+import IDAlogo from './assets/company_logo/IDA_logo.jpg';
+import edunetlogo from './assets/company_logo/edunet_logo.png';
+import medtoureasy from './assets/company_logo/medtoureasyy_logo.jpg';
+
+// Education Section Logo's
+import hyulogo from './assets/education_logo/hyu_logo.png';
+import ssssmvlogo from './assets/education_logo/ssssmv_logo.jpg';
+import kvslogo from './assets/education_logo/kvs_logo.jpg';
+
+// Project Section Logo's
+import Profilelogo from './assets/work_logo/Profile.jpeg';
+import AAClogo from './assets/work_logo/AAC_Logo.jpeg';
+import camplogo from './assets/work_logo/camp_logo.jpeg';
+import Sciencelogo from './assets/work_logo/Science_logo.jpg';
+
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'GSAP', logo: gsapLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: Shadowfoxlogo,
+      role: "Web Development Intern",
+      company: "Shadowfox",
+      date: "May 2026 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "React JS",
+        "TypeScript",
+        "Node JS",
+        "Tailwind CSS",
+        "MongoDb",
+        "Redux",
+        " Next Js",
+      ],
+    },
+    {
+      id: 1,
+      img: IDAlogo,
+      role: "Mathematics Educator",
+      company: "Ideal Defence Academy",
+      date: "April 2026 - Present",
+      desc: "As a Mathematics and Computer Science Educator at Ideal Defence Academy, I am responsible for teaching and mentoring students in various subjects, including mathematics, computer science, and programming. I design and deliver engaging lessons, create educational materials, and provide personalized support to help students excel in their studies and prepare for competitive exams.",
+      skills: [
+        "Special Education",
+        "Coaching",
+        "Mentoring",
+        "Problem Solving",
+        "Communication",
+        "Classroom Management",
+        "Curriculum Development",
+      ],
+    },
+    {
+      id: 2,
+      img: edunetlogo,
+      role: "Frontend Intern",
+      company: "Edunet Foundation",
+      date: "July 2025 - October 2025",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "Javascript",
+        "Bootstrap",
+        "Figma",
+        "Material UI",
+      ],
+    },
+    {
+      id: 3,
+      img: medtoureasy,
+      role: "Data Analytics Trainee",
+      company: "MedToureasy",
+      date: "November 2025 - December 2025",
+      desc: "As a Data Analytics Trainee at MedToureasy, I was responsible for analyzing and interpreting complex datasets to provide actionable insights that informed business decisions. I utilized tools such as Excel, SQL, and Python to clean, visualize, and analyze data related to customer behavior, market trends, and operational performance. My work contributed to optimizing marketing strategies and improving overall business efficiency.",
+      skills: [
+        "Data Analysis",
+        "SQL",
+        "Python",
+        "Excel",
+        "Lead Generation",
+      ],
+    }
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: hyulogo,
+      school: "Hemchand Yadav University, Durg, Chhattisgarh",
+      date: "Aug 2025 - Present",
+      grade: "7.1 CGPA",
+      desc: "Currently pursuing my Master's degree in Computer Science (M.Sc.) at Hemchand Yadav University, Durg, Chhattisgarh. My coursework includes advanced topics such as Data Structures and Algorithms, Database Management Systems, Web Development, and Software Engineering. I am actively involved in research projects and practical applications of computer science concepts, further enhancing my knowledge and skills in the field.",
+      degree: "Master of Science - MSc (Computer Science)",
+    },
+    {
+      id: 1,
+      img: ssssmvlogo,
+      school: "Swami Shri Swaroopanand Saraswati Mahavidyalaya, Bhilai, Chhattisgarh",
+      date: "Sept 2022 - June 2025",
+      grade: "6.84 CGPA",
+      desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from Swami Shri Swaroopanand Saraswati Mahavidyalaya, Bhilai, Chhattisgarh. During my undergraduate studies, I gained a strong foundation in computer science principles, programming languages, and software development. I actively participated in various projects and internships that allowed me to apply theoretical knowledge to real-world scenarios, further enhancing my skills and preparing me for a successful career in the tech industry.",
+      degree: "Bachelor of Science - BSC (Computer Science)",
+    },
+    {
+      id: 2,
+      img: kvslogo,
+      school: "Kendriya Vidyalaya, Durg, Chhattisgarh",
+      date: "Apr 2021 - April 2022",
+      grade: "77.4%",
+      desc: "I completed my class 12 education from Kendriya Vidyalaya, Durg, under the CBSE board, where I studied Science with PCM (Physics, Chemistry, Mathematics). This academic experience provided me with a strong foundation in scientific principles and analytical thinking, which has been instrumental in shaping my interest and skills in the field of computer science.",
+      degree: "CBSE(XII) - Science stream(PCM)",
+    },
+    {
+      id: 3,
+      img: kvslogo,
+      school: "Kendriya Vidyalaya, Durg, Chhattisgarh",
+      date: "Apr 2019 - April 2020",
+      grade: "80.5%",
+      desc: "I completed my class 10 education from Kendriya Vidyalaya, Durg, under the CBSE board, where I studied Science with Computer.",
+      degree: "CBSE(X), Science with Computer Application",
+    },
+  ];
+  
+  export const Achivements = [
+    {
+      id: 0,
+      title: "NCC Commanding Officer Commendation 2025",
+      description:
+        "Received the prestigious NCC Commanding Officer Commendation in 2025 for outstanding leadership and dedication as a cadet in the National Cadet Corps (NCC). This recognition highlights my commitment to discipline, teamwork, and service, reflecting my ability to excel in challenging environments and contribute positively to the community.",
+      image: Profilelogo,
+      tags: ["Leadership", "Discipline", "Service", "Decision-Making", "Excellence"],
+      Drive: "https://drive.google.com/file/d/1i0q3wKaQ-wMoTfugmBaUStWMiMc-rVHk/view?usp=sharing",
+    },
+    {
+      id: 1,
+      title: "Army Attachment Camp 2025 - Overall Excellence in Training",
+      description:
+        "Awarded for Overall Excellence in Training during the Army Attachment Camp in 2025, recognizing my exceptional performance, dedication, and leadership skills throughout the rigorous training program. This accolade reflects my ability to excel in a demanding military environment and contribute effectively to team success.",
+      image: AAClogo,
+      tags: ["Military Training", "Weapon Handling", "Map Reading", "Field Craft", "Battlefield Skills", "Drills", "Ammunition Handling"],
+      Drive:"https://drive.google.com/file/d/1AQiaoCaIC_Gu6mR8RjULGY0jq73SbS7M/view?usp=sharing",
+      
+    },
+    {
+      id: 2,
+      title: "Annual Training Camp 2024 - Camp Senior Under Officer (CSUO)",
+      description:
+        "Recognized for outstanding performance and leadership during the Annual Training Camp 2024, where I served as a Camp Senior Under Officer (CSUO). This experience enhanced my tactical skills and team management capabilities.",
+      image: camplogo,
+      tags: ["Team Management", "Organizing Abilities", "Leadership", "Officer Like Qualities", "Decision-Making", "Time Management", "Problem-Solving"],
+      Drive: "https://drive.google.com/file/d/1Bm53-mGal4VEDbUlz8_mNRrhsLBniHdO/view?usp=sharing",
+    },
+    {
+      id: 3,
+      title: "State Science Fair 2025 - Best Innovation Award",
+      description:
+        "Received the Best Innovation Award at the State Science Fair 2025 for developing a HC-05 bluetooth based arduino car that can be controlled through a mobile application. This project showcased my creativity, technical skills, and ability to apply scientific principles to solve real-world problems.",
+      image: Sciencelogo,
+      tags: ["Innovation", "Scientific Approach", "Project Development", "Problem-Solving", "Technical Skills", "Real-World Application"],
+      Drive: "https://drive.google.com/file/d/1fD_VDjkRG_87bBocwN8_T9DBeG4iQDre/view?usp=sharing",
+    },
+    
+  ];  
